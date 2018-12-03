1bab12fa43c0ea5925c1187610c3a4a2fb9f55c7
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2018-12-02 19:23:09 -0700

    Merge master to antigen. Squashed commit of the following:
    commit e1f7776c71a5be2a6a903c180e65dc64b0b2b51e
    Merge: 27a9b9d 6b08912
    Author: Umar Bari <ubari94@gmail.com>
    Date:   Sun Dec 2 19:02:45 2018 -0700
    
        Merge pull request #2 from Clumsy-Coder/zsh-init
    
        Zsh init
    
    commit 6b089123a5d71498b6b61ef7647fd86e1d8a93e7
    Author: Clumsy-Coder <ubari94@gmail.com>
    Date:   Sun Dec 2 18:57:58 2018 -0700
    
        Update all README to have update shell filenames
    
    commit e41544e4bf52379f0c59bdeae53e9f60307eac1d
    Author: Clumsy-Coder <ubari94@gmail.com>
    Date:   Sun Dec 2 18:40:31 2018 -0700
    
        Rename sh files and update their imports
    
        Rename:
        - alias/index.sh to alias/alias.sh
        - environment/index.sh to environment/environment.sh
        - tools/index.sh to tools/tools.sh
        - tools/tmux/index.sh to tools/tmux/tmux.sh
        - tools/vim/index.sh to tools/vim/vim.sh
    
        Update shell file imports:
        Update the filename paths so they get imported properly

--- index.sh
+++ index.sh
@@ -1,11 +1,11 @@
 # import aliases
-source ~/zsh-terminal-config/alias/index
+source ~/zsh-terminal-config/alias/alias.sh
 
 # import environment variables
-source ~/zsh-terminal/environment/index
+source ~/zsh-terminal/environment/environment.sh
 
 # import tools config
-source ~/zsh-terminal-config/tools/index
+source ~/zsh-terminal-config/tools/tools.sh
 
 # run command after loading config and settings
 echo hello $(whoami)
