0e1151987928c3b7d4bd2e15e199848b589c91cc
Clumsy-Coder <ubari94@gmail.com>
2018-12-02 20:20:40 -0700

    Import powerlevel9k theme config to environment.sh

--- environment/environment.sh
+++ environment/environment.sh
@@ -1,2 +1,6 @@
 # import all environment based settings and config
-source ~/zsh-terminal-config/environment/antigen/index.sh
+# import powerlevel9k theme settings/config before importing antigen
+source ~/zsh-terminal-config/environment/theme/powerlevel9k.sh
+
+# import antigen configs
+source ~/zsh-terminal-config/environment/antigen/antigen.sh
