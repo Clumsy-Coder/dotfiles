fff2f5d93a53bad8ad83c5e7884f77785c728df9
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2018-12-02 20:42:28 -0700

    Update powerlevel9k theme config file path for environment.sh

--- environment/environment.sh
+++ environment/environment.sh
@@ -1,6 +1,6 @@
 # import all environment based settings and config
 # import powerlevel9k theme settings/config before importing antigen
-source ~/zsh-terminal-config/environment/theme/powerlevel9k.sh
+source ~/zsh-terminal-config/environment/theme/powerlevel9k/powerlevel9k.sh
 
 # import antigen configs
 source ~/zsh-terminal-config/environment/antigen/antigen.sh
