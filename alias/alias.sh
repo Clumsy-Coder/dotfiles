bf0e3eab77bb594651b009706c741b83d62432f9
Clumsy-Coder <ubari94@gmail.com>
2019-01-06 16:33:04 -0700

    Add alias to when using 'ls' command
    uses the 'coreutils' GNU library
    Calls the command 'gls'
    
    So it can use '--group-directories-first' argument

--- alias/alias.sh
+++ alias/alias.sh
@@ -1,4 +1,5 @@
 # import alias
+alias ls='gls --color'    # need 'coreutils' package. install with brew install coreutils
 alias l='ls -lah --group-directories-first'
 alias cls='clear'
 alias mkcd='function _mkcd(){ mkdir $1; cd $1;}; _mkcd'   # make a dir and cd to it. mkcd newDir
