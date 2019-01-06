df68dfd21917c219ef1e81fe6d660bd6c13fa415
Clumsy-Coder <ubari94@gmail.com>
2019-01-06 16:35:55 -0700

    Import the alias.sh as the last script in index.sh
    If alias.sh was loaded first, the custom aliases would be overwritten.

--- index.sh
+++ index.sh
@@ -1,11 +1,11 @@
-# import aliases
-source ~/zsh-terminal-config/alias/alias.sh
-
 # import environment variables
 source ~/zsh-terminal/environment/environment.sh
 
 # import tools config
 source ~/zsh-terminal-config/tools/tools.sh
+
+# import aliases
+source ~/zsh-terminal-config/alias/alias.sh
 
 # run command after loading config and settings
 echo hello $(whoami)
