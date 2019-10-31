391f0282ae0da1bd6789bac4a42a08b86da646ca
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2019-10-31 12:29:41 -0600

    Split alias.sh by OS

---
+++ alias/debian/alias.sh
@@ -0,0 +1,12 @@
+# directory alias
+alias documents='cd ~/Documents'
+alias downloads='cd ~/Downloads'
+alias desktop='cd ~/Desktop'
+
+# command alias
+alias cls='clear'
+alias l='ls -lah --group-directories-first'
+alias ls-all-dir='ls -la | grep "^d"'   #list all folders, even the hidden ones
+alias ls-dir='ls -l | grep "^d"'        #list all folders
+alias mkcd='function _mkcd(){ mkdir $1; cd $1;};_mkcd'			#make a directory and go into it
+alias cntFiles='ls -F |grep -v / | wc -l'
