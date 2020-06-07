2b8e99f23d62e04288d4ced091957325cee6b6e1
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2020-06-07 14:14:45 -0600

    Add alias to go to previous directory

--- alias/osx/alias.sh
+++ alias/osx/alias.sh
@@ -27,6 +27,7 @@ alias ls-all-dir='ls -la | grep "^d"'   #list all folders, even the hidden ones
 alias ls-dir='ls -l | grep "^d"'        #list all folders
 alias mkcd='function _mkcd(){ mkdir $1; cd $1;};_mkcd'			#make a directory and go into it
 alias cntFiles='ls -F |grep -v / | wc -l'
+alias ..='cd ..'
 
 #git alias
 # alias gits='git status'
