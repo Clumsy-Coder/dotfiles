482bb86965d3b0b3d4f12d53690fea30b7d89fc9
Clumsy-Coder <ubari94@gmail.com>
2020-08-14 15:13:14 -0600

    feat(setup): create a setup script for debian based system
    currently it
    - updates apt repos
    - installs full upgrade (OS update)
    - read from applist file (<projectroot>/apps/debian/applist)
    - update apt repos
    - upgrade pending apps
    - clean apt cache
    
    TODO: implement script to install complex software. Ex: docker, nvm, node

---
+++ setup/debian/install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# update and upgrade
+sudo apt update && sudo apt full-upgrade -y
+
+# install programs
+while read line; do sudo apt install $line; done < ../../apps/debian/applist
+
+# run all scripts from ./programs/ folder
+# for f in programs/*.sh; do bash "$f" -H; done
+
+sudo apt update
+sudo apt upgrade -y
+sudo apt autoremove -y
