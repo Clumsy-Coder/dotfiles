499426d549f7a4552191ad164bdbb4ff5d572977
Clumsy-Coder <ubari94@gmail.com>
2021-10-02 09:50:49 -0600

    feat(setup): add script to install terminal based and GUI based software
    Ask for user confirmation to install.

--- setup/debian/install.sh
+++ setup/debian/install.sh
@@ -6,8 +6,23 @@ sudo apt update && sudo apt full-upgrade -y
 # install programs. Use ../../apps/debian/applist
 while read line; do sudo apt install $line -y; done < ../../apps/debian/applist
 
-# run all scripts from ./programs/ folder
-for f in programs/*.sh; do bash "$f" -H; done
+read -p "Do want to install terminal based software (y/n)? " -n 1 -r installTerminalSoft
+echo    # (optional) move to a new line
+read -p "Do want to install GUI based software (y/n)? " -n 1 -r installGuiSoft
+echo    # (optional) move to a new line
+echo    # (optional) move to a new line
+
+if [[ $installTerminalSoft =~ ^[Yy]$ ]]
+then
+    echo "installing terminal based software";
+    for f in ./programs/terminal-based/*.sh; do bash "$f" -H; done
+fi
+
+if [[ $installGuiSoft =~ ^[Yy]$ ]]
+then
+    echo "installing GUI based software";
+    for f in ./programs/gui-based/*.sh; do bash "$f" -H; done
+fi
 
 sudo apt update
 sudo apt upgrade -y
