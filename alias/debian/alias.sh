8fa61591661e0d0f7cb70e6b647a4d90720f00d2
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2021-10-02 09:47:59 -0600

    chore(alias): add alias to display current directory contents using 'exa' for debian OS

--- alias/debian/alias.sh
+++ alias/debian/alias.sh
@@ -6,7 +6,8 @@ alias desktop='cd ~/Desktop'
 # command alias
 alias cls='clear'
 alias l='ls -lah --group-directories-first'
-alias ls-all-dir='ls -la | grep "^d"'   #list all folders, even the hidden ones
-alias ls-dir='ls -l | grep "^d"'        #list all folders
-alias mkcd='function _mkcd(){ mkdir $1; cd $1;};_mkcd'			#make a directory and go into it
+alias ls-all-dir='ls -la | grep "^d"'                       #list all folders, even the hidden ones
+alias ls-dir='ls -l | grep "^d"'                            #list all folders
+alias mkcd='function _mkcd(){ mkdir $1; cd $1;};_mkcd'      #make a directory and go into it
 alias cntFiles='ls -F |grep -v / | wc -l'
+alias lse='exa -lahFHmguU --git --group-directories-first'  # use 'exa' to view directory contents
