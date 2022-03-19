9cd8645c1655af8a1d1b463faa14cbacde525f76
Clumsy-Coder <ubari94@gmail.com>
2022-03-19 10:23:20 -0600

    chore(vim): create symbolic links for neovim using jesseleite/vim-sourcery specs
    These symbolic links are needed for jesseleite/vim-sourcery and other plugins, config and key
    mappings  to be loaded correctly

--- tools/neovim/neovim.sh
+++ tools/neovim/neovim.sh
@@ -1,2 +1,6 @@
 mkdir -p ~/.config/nvim
-ln -f ~/dotfiles/tools/neovim/init.vim ~/.config/nvim
+# ln -s ~/dotfiles/tools/neovim/init.vim ~/.config/nvim
+ln -sf ~/dotfiles/tools/vim/.vimrc ~/.config/nvim/init.vim
+ln -sf ~/dotfiles/tools/vim/mappings.vim ~/.config/nvim
+ln -sf ~/dotfiles/tools/vim/plugins.vim ~/.config/nvim
+ln -sf ~/dotfiles/tools/vim/config/ ~/.config/nvim
