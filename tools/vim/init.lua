97290b7ac7e27bb19cd8ac3b062189bd1da9d67f
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2022-04-06 11:50:05 -0600

    feat(neovim): add plugin lewis6991/impatient.nvim
    Will be used for improving neovim startup time

--- tools/vim/init.lua
+++ tools/vim/init.lua
@@ -1,3 +1,10 @@
+-- lewis6991/impatient.nvim plugin is required to be loaded first to improve startup time, as per instructions
+-- https://github.com/lewis6991/impatient.nvim#setup
+local impatient_status_ok, impatient = pcall(require, "impatient")
+if impatient_status_ok then                     -- using pcall to prevent errors when setting up neovim for the first time
+  require('impatient').enable_profile()
+end
+
 require('config.general-config')
 require('config.keymaps')
 require('config.plugins')
