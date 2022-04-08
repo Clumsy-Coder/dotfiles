15acb590c6cc2bcfc91f4bf1cfee54d8ba953cf1
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2022-04-08 10:24:30 -0600

    refactor(neovim): write importing plugin config files in a simpler manner

--- tools/neovim/lua/config/load-plugin-config.lua
+++ tools/neovim/lua/config/load-plugin-config.lua
@@ -1,22 +1,22 @@
 -- load plugins config here
 
-require('config.telescope')
-require('config.cmp')
-require('config.cmp-git')
-require('config.lsp')
-require('config.treesitter')
-require('config.which-key')
-require('config.toggleterm')
-require('config.bufferline')
-require('config.nvim-tree')
-require('config.symbols-outline')
-require('config.gitsigns')
-require('config.comment')
-require('config.autopairs')
-require('config.alpha')
-require('config.project')
+require "config.telescope"
+require "config.cmp"
+require "config.cmp-git"
+require "config.lsp"
+require "config.treesitter"
+require "config.which-key"
+require "config.toggleterm"
+require "config.bufferline"
+require "config.nvim-tree"
+require "config.symbols-outline"
+require "config.gitsigns"
+require "config.comment"
+require "config.autopairs"
+require "config.alpha"
+require "config.project"
 
-require('config.theme')
-require('config.indent-blankline')
-require('config.colorizer')
-require('config.lualine')
+require "config.theme"
+require "config.indent-blankline"
+require "config.colorizer"
+require "config.lualine"
