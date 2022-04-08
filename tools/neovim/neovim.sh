c8a90ac3ecf452fddb908d571f32068d85feeb02
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2022-04-07 19:42:57 -0600

    chore(neovim): remove deprecated neovim folder
    will be replaced by the new one

--- tools/neovim/neovim.sh
+++
@@ -1,6 +0,0 @@
-mkdir -p ~/.config/nvim
-# ln -s ~/dotfiles/tools/neovim/init.vim ~/.config/nvim
-ln -sf ~/dotfiles/tools/vim/.vimrc ~/.config/nvim/init.vim
-ln -sf ~/dotfiles/tools/vim/mappings.vim ~/.config/nvim
-ln -sf ~/dotfiles/tools/vim/plugins.vim ~/.config/nvim
-ln -sf ~/dotfiles/tools/vim/config/ ~/.config/nvim
