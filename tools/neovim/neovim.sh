145670eaadec93a54f07bc5206cfc0161d2beb66
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2022-04-17 16:35:34 -0600

    chore: remove unused .sh files
    These files are either:
    - loads configs. Ex: loading powerlevel9k configs
    - create symbolic links for tools. Ex: create symbolic link for neovim
    
    will be handled by dotbot for creating symbolic links
    loading configs will be handled in zshrc or zshenv
    
    This is to remove unnecessary files

--- tools/neovim/neovim.sh
+++
@@ -1,4 +0,0 @@
-mkdir -p ~/.config/nvim
-
-ln -sf ~/dotfiles/tools/neovim/init.lua ~/.config/nvim/init.lua
-ln -sf ~/dotfiles/tools/neovim/lua/ ~/.config/nvim
--- tools/tmux/tmux.sh
+++
@@ -1,2 +0,0 @@
-# create a hard link to ~/tmux.conf
-ln -sf ~/dotfiles/tools/tmux/tmux.conf ~/.tmux.conf
