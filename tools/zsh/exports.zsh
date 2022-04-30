f6fb2dc5e06e2830f06c07f90c227addc2e68a94
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2022-04-29 19:56:47 -0600

    feat(zsh): add rust to PATH
      ## what
    - add rust bin folder to PATH
    
      ## why
    - to be able to run rust packages

--- tools/zsh/exports.zsh
+++ tools/zsh/exports.zsh
@@ -19,6 +19,10 @@ export PYTHONIOENCODING='UTF-8';
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 
+  # export Rust to PATH
+  # Rust installed using dotbot script dotfiles/meta/configs/rust.yaml file
+  export PATH="$HOME/.cargo/bin:$PATH"
+
   # export golang to PATH
   # https://stackoverflow.com/a/13223578/3053548
   export GOPATH=~/go
