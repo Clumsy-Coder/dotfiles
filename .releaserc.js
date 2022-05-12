0c908923a35635b5032da94f13f7d2eb0fe00608
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2022-05-12 11:13:54 -0600

    refactor(semantic-release): update .releaserc.js to work with github-actions
      ## what
    - remove plugin @google/semantic-release-replace-plugin
    - remove releaseRules
    - add which commit type is displayed in CHANGELOG.md
    
      ## how
    
      ## why
    - remove plugin @google/semantic-release-replace-plugin
      - no longer needed since package.json is removed
      - check 597ce88
    - remove 'releaseRules' property
      - use default Angular commit type release rules
        - check https://github.com/semantic-release/commit-analyzer/blob/master/lib/default-release-rules.js
        - commit types for release
          - feat : MINOR release
          - fix  : PATCH release
          - perf : PATCH release
    - add which commit type is displayed in CHANGELOG.md
      - ONLY display commit types that match release rules. Others are
        ommited from CHANGELOG.md
        - feat
        - fix
        - perf
    
      ## where
    - .releaserc.js
    
      ## usage

--- .releaserc.js
+++ .releaserc.js
@@ -5,47 +5,38 @@ module.exports = {
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
     "@semantic-release/changelog",
-    [
-      "@google/semantic-release-replace-plugin",
-      {
-        replacements: [
-          {
-            files: ["package.json"],
-            from: '"version": ".*"',
-            to: '"version": "${nextRelease.version}"',
-          },
-        ],
-      },
-    ],
     "@semantic-release/github",
     [
       "@semantic-release/git",
       {
-        assets: ["CHANGELOG.md", "package.json"],
+        assets: ["CHANGELOG.md"],
       },
     ],
   ],
-  analyzeCommits: {
-    releaseRules: [
-      { type: "docs", release: "patch" },
-      { type: "build", release: "patch" },
-    ],
-  },
+  // use https://github.com/conventional-changelog/conventional-changelog/tree/master/packages/conventional-changelog-conventionalcommits
+  // release rules: https://github.com/semantic-release/commit-analyzer/blob/master/lib/default-release-rules.js
   preset: "conventionalcommits",
+
+  // git emoji obtained from
+  //    https://gist.github.com/parmentf/035de27d6ed1dce0b36a
+  //    https://github.com/caiyongji/emoji-list
+  //    https://github.com/pvdlg/conventional-changelog-metahub#commit-types
+  // preset config for generating release notes and changelogs
+  //    https://github.com/conventional-changelog/conventional-changelog-config-spec/blob/master/versions/2.0.0/README.md#type
+  // prettier-ignore
   presetConfig: {
     types: [
-      { type: "feat", section: "Features" },
-      { type: "feature", section: "Features" },
-      { type: "fix", section: "Bug Fixes" },
-      { type: "perf", section: "Performance Improvements" },
-      { type: "revert", section: "Reverts" },
-      { type: "docs", section: "Documentation" },
-      { type: "style", section: "Styles" },
-      { type: "chore", section: "Miscellaneous Chores" },
-      { type: "refactor", section: "Code Refactoring" },
-      { type: "test", section: "Tests" },
-      { type: "build", section: "Build System" },
-      { type: "ci", section: "Continuous Integration" },
+      { type: "feat",     section: ":sparkles: Features",                             hidden: false }, // MINOR
+      { type: "fix",      section: ":bug: Bug Fixes",                                 hidden: false }, // PATCH
+      { type: "perf",     section: ":zap: Performance Improvements",                  hidden: false }, // PATCH
+      { type: "revert",   section: ":rewind: Reverts",                                hidden: true  }, // NO_RELEASE
+      { type: "docs",     section: ":books: Documentation",                           hidden: true  }, // NO_RELEASE
+      { type: "style",    section: ":lipstick: Styles",                               hidden: true  }, // NO_RELEASE
+      { type: "chore",    section: ":octopus: Miscellaneous Chores",                  hidden: true  }, // NO_RELEASE
+      { type: "refactor", section: ":recycle: Code Refactoring",                      hidden: true  }, // NO_RELEASE
+      { type: "test",     section: ":white_check_mark: Tests",                        hidden: true  }, // NO_RELEASE
+      { type: "build",    section: ":package: Build System",                          hidden: true  }, // NO_RELEASE
+      { type: "ci",       section: ":construction_worker: Continuous Integration",    hidden: true  }, // NO_RELEASE
     ],
   },
 };
