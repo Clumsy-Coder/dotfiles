c5b10e2589d3c98a4847eefb7a52e62c6e3308ad
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2022-07-06 17:38:09 -0600

    feat(astronvim): add plugin 'hrsh7th/cmp-cmdline'

---
+++ tools/astronvim/plugins/init.lua
@@ -0,0 +1,30 @@
+-- install, enable, disable plugins
+--
+-- inspired by
+--    https://github.com/AstroNvim/AstroNvim/blob/65c6e6c431/lua/core/plugins.lua
+--    https://code.mehalter.com/projects/68/files/master/plugins/init.lua
+--    https://github.com/datamonsterr/astronvim_config/blob/main/plugins/init.lua
+--    https://github.com/hunger/AstroVim/blob/my_config/lua/core/plugins.lua
+
+return {
+  -- //////////////////////////////////////////////////////////////////////////////////////////// --
+  -- disable plugins
+
+  -- //////////////////////////////////////////////////////////////////////////////////////////// --
+  -- install plugins
+
+  -- FUNCTIONAL PLUGINS
+
+  -- disable lazy loading for friendly snippets
+  -- that triggers the loading of cmp
+  -- ["rafamadriz/friendly-snippets"] = { event = { nil } },
+  -- ["hrsh7th/nvim-cmp"] = { event = { nil } },
+
+  -- cmp code completion
+  [ "hrsh7th/cmp-cmdline" ]   = {           -- cmdline completions
+    after = "nvim-cmp",
+    config = function()
+      astronvim.add_user_cmp_source("cmdline")
+    end
+  },
+}
