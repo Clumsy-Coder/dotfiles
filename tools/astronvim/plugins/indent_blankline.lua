1f3aa4d50619a17df697a3fab0ea1bb200cd0c7d
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2022-07-12 17:53:42 -0600

    feat(astronvim): add plugin config for 'lukas-reineke/indent-blankline.nvim'

---
+++ tools/astronvim/plugins/indent_blankline.lua
@@ -0,0 +1,20 @@
+-- Config: indent-blankline
+-- lukas-reineke/indent-blankline.nvim
+
+return {
+  use_treesitter = true,
+  show_current_context = true,
+  show_current_context_start = true,
+  indent_level = 50,
+  filetype_exclude = {
+    'dashboard',
+    'toggleterm',
+    'packer',
+    'help',
+    'NvimTree',
+    'alpha'
+  }
+}
+
+-- for more options, search for 'options.' in
+-- https://github.com/lukas-reineke/indent-blankline.nvim/blob/master/lua/indent_blankline/init.lua
