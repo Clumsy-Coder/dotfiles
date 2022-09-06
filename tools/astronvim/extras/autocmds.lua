08a8374ecb2e4680b8a3e089fb5a88caf72d2787
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2022-09-06 17:35:02 -0600

    style(astronvim): fix stylua issues for 'autocmds.lua'

--- tools/astronvim/extras/autocmds.lua
+++ tools/astronvim/extras/autocmds.lua
@@ -6,25 +6,26 @@
 -- https://github.com/hunger/AstroVim/blob/my_config/lua/core/autocmds.lua
 
 local is_available = astronvim.is_available
-local cmd = vim.api.nvim_create_autocmd
-local augroup = vim.api.nvim_create_augroup
+local create_autocmd = vim.api.nvim_create_autocmd
+local create_augroup = vim.api.nvim_create_augroup
 local create_command = vim.api.nvim_create_user_command
 
 -- ////////////////////////////////////////////////////////////////////////////////////////////// --
 -- auto install TreeSitter languages when opening a file
 -- obtained from https://github.com/nvim-treesitter/nvim-treesitter/issues/2108#issuecomment-993642212
-augroup("NvimTreeSitter-autoinstall", { clear = true })
-cmd("FileType", {
+create_augroup("NvimTreeSitter-autoinstall", { clear = true })
+create_autocmd("FileType", {
   desc = "Auto-install TreeSitter language",
   group = "NvimTreeSitter-autoinstall",
   pattern = "*",
   callback = function()
-    local parsers = require('nvim-treesitter.parsers')
+    local parsers = require("nvim-treesitter.parsers")
     local lang = parsers.get_buf_lang()
     if parsers.get_parser_configs()[lang] and not parsers.has_parser(lang) then
       vim.schedule_wrap(function()
-        vim.cmd("TSInstallSync! " .. lang)   -- using TSInstallSync! instead of TSInstallSync because it will prompt you to overwrite existing language
-        vim.cmd[[e!]]
+        -- using TSInstallSync! instead of TSInstallSync because it will prompt you to overwrite existing language
+        vim.cmd("TSInstallSync! " .. lang)
+        vim.cmd([[e!]])
       end)()
     end
   end,
@@ -32,12 +33,12 @@ cmd("FileType", {
 
 -- highlight yanked text
 -- obtained from https://stackoverflow.com/a/73365602/3053548
-augroup('highlight_yank', { clear = false })
-cmd('TextYankPost', {
-  group = 'highlight_yank',
-  desc = 'Hightlight selection on yank',
-  pattern = '*',
+create_augroup("highlight_yank", { clear = false })
+create_autocmd("TextYankPost", {
+  group = "highlight_yank",
+  desc = "Highlight selection on yank",
+  pattern = "*",
   callback = function()
-    vim.highlight.on_yank({ higroup = 'IncSearch', timeout = 500 })
+    vim.highlight.on_yank({ higroup = "IncSearch", timeout = 500 })
   end,
 })
