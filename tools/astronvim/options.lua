ee7c3ecdd5a2aef2277a043a8b25014bf05b488e
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2022-09-06 15:15:37 -0600

    feat(astronvim): change cursor behaviour when going to INSERT mode
      ## what
      - change the cursor behaviour when going to INSERT mode
        - change from line to block cursor
    
      ## how
    
      ## why
      - the line cursor barely visible when going to INSERT mode.
    
      ## where
    
      ## usage

---
+++ tools/astronvim/options.lua
@@ -0,0 +1,14 @@
+return {
+  opt = {
+    -- setting cursor style in different modes
+    -- check current guicursor value with command ':set guicursor'
+    -- https://github.com/AstroNvim/AstroNvim/issues/903#issuecomment-1219860755
+    -- https://vim.fandom.com/wiki/Configuring_the_cursor
+    -- i = insert
+    -- v = visual
+    -- c = command
+    -- n = normal
+    guicursor = "n-v-c-i-sm:block,ci-ve:ver25,r-cr-o:hor20",
+    cursorline = true
+  }
+}
