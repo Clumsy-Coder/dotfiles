3b3bf26798d9c1ea6568d631884aecaae3aa3de8
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2022-09-06 17:35:28 -0600

    feat(astronvim): display hidden files when using telescope

---
+++ tools/astronvim/plugins/telescope.lua
@@ -0,0 +1,13 @@
+-- Config: telescope
+-- https://github.com/nvim-telescope/telescope.nvim
+--
+-- obtained from
+--    https://github.com/nvim-treesitter/nvim-treesitter
+
+return {
+  pickers = {
+    find_files = {
+      hidden = true, -- show hidden files
+    },
+  },
+}
