8da735b031337c43bbed837e9ea0d7ec092c8618
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2022-09-06 19:12:21 -0600

    feat(astronvim): add plugin config for 'williamboman/mason-lspconfig.nvim'

---
+++ tools/astronvim/plugins/mason-lspconfig.lua
@@ -0,0 +1,10 @@
+-- Config: mason-lspconfig
+-- williamboman/mason-lspconfig.nvim
+--
+-- obtained from
+--    https://code.mehalter.com/projects/68/files/master/plugins/mason-lspconfig.lua
+
+return {
+  automatic_installation = true,
+}
+
