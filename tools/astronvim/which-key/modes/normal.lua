63c6041a8b0367fa17b5d2b40560a7f087aad5b9
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2022-09-07 16:51:38 -0600

    feat(astronvim): add which-key bindings for plugin 'akinsho/git-conflict.nvim'
      ## what
      - add which-key bindings for plugin 'akinsho/git-conflict.nvim'
    
      ## how
    
      ## why
      - using which-key makes it easy to remember the keybindings for 'git-conflict'
    
      ## where
    
      ## usage
      which key bindings in NORMAL mode:
      - Cc
        - take current change
      - Ci
        - take their changes
      - Cb
        - take both changes
      - C0
        - take none
      - Cr
        - refresh git conflict hunks
      - Cl
        - list git conflict hunks
      - C]
        - go to next git conflict hunk
      - C[
        - go to previose git conflict hunk

---
+++ tools/astronvim/which-key/modes/normal.lua
@@ -0,0 +1,19 @@
+-- Which key keymaps
+-- mode: NORMAL
+--
+-- inspired by:
+--    https://github.com/thieung/dotfiles/blob/main/config/nvim/which-key/mode/n.lua
+
+return {
+  ["C"] = {
+    name = "git-conflict",
+    ["c"] = { "<Plug>(git-conflict-ours)", "Take Current" },
+    ["i"] = { "<Plug>(git-conflict-theirs)", "Take Incoming" },
+    ["b"] = { "<Plug>(git-conflict-both)", "Take Both" },
+    ["0"] = { "<Plug>(git-conflict-none)", "Take None" },
+    ["r"] = { "<cmd>GitConflictRefresh<cr>", "Refresh Conflicts" },
+    ["l"] = { "<cmd>GitConflictListQf<cr>", "List Conflicts" },
+    ["]"] = { "<Plug>(git-conflict-next-conflict)",  "Next conflict" },
+    ["["] = { "<Plug>(git-conflict-prev-conflict)",  "Previous conflict" },
+  },
+}
---
+++ tools/astronvim/which-key/register_mappings.lua
@@ -0,0 +1,8 @@
+-- Config: which-key
+-- folke/which-key.nvim
+
+local normal_mode_keymaps = require("user.which-key.modes.normal")
+
+return {
+  n = normal_mode_keymaps
+}
