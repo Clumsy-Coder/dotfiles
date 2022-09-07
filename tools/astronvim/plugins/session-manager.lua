34182c41032af58025ea33a0b5e6aa994e80f9a3
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2022-09-07 17:09:48 -0600

    feat(astronvim): add plugin config for 'Shatur/neovim-session-manager'

---
+++ tools/astronvim/plugins/session-manager.lua
@@ -0,0 +1,9 @@
+-- Config: session-manager
+-- Shatur/neovim-session-manager
+--
+-- obtained from:
+--    https://github.com/datamonsterr/astronvim_config/blob/main/plugins/session_manager.lua
+
+return {
+  autosave_last_session = true,
+}
