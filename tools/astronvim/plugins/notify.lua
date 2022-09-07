ff530681cf3f6eb301ab638d03a085dfc57b6726
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2022-09-07 17:10:16 -0600

    feat(astronvim): add plugin config for 'rcarriga/nvim-notify'

---
+++ tools/astronvim/plugins/notify.lua
@@ -0,0 +1,11 @@
+-- Config: notify
+-- rcarriga/nvim-notify
+--
+-- obtained from
+--    https://github.com/datamonsterr/astronvim_config/blob/main/plugins/notify.lua
+
+return {
+  timeout = 3000,
+  opacity = 0,
+  background_colour = "#000000",
+}
