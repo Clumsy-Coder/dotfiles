6f3f2a8b4bb8bd02b425659c38e6873fc10faa94
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2022-09-06 19:18:20 -0600

    feat(astronvim): add plugin config for 'WhoIsSethDaniel/mason-tool-installer.nvim'

---
+++ tools/astronvim/plugins/mason-tool-installer.lua
@@ -0,0 +1,24 @@
+-- Config: mason-tool-installer
+-- WhoIsSethDaniel/mason-tool-installer.nvim
+--
+-- obtained from
+--    https://code.mehalter.com/projects/68/files/master/plugins/mason-tool-installer.lua
+
+return {
+  auto_update = true,
+  ensure_installed = {
+    "actionlint",       -- Github actions linter
+    "codespell",        -- spell check
+    "eslint_d",         -- eslint but faster
+    "gitlint",          -- git commit message linter
+    "hadolint",         -- dockerfile linter
+    "luacheck",         -- lua linter
+    "markdownlint",     -- markdown linter
+    "misspell",         -- check commonly misspelled words
+    "prettierd",        -- prettier but faster
+    "shellcheck",       -- shell linter
+    "stylua",           -- lua code formatter
+    "vint",             -- vimscript linter
+    "yamllint"          -- YAML linter
+  },
+}
