eb61f621f0c854554cf01cac22c345a2d65f8add
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2022-09-20 09:41:08 -0600

    docs(astronvim:mason): add comment on how config property 'automatic_installation' works

--- tools/astronvim/plugins/mason-lspconfig.lua
+++ tools/astronvim/plugins/mason-lspconfig.lua
@@ -5,6 +5,7 @@
 --    https://code.mehalter.com/projects/68/files/master/plugins/mason-lspconfig.lua
 
 return {
+  -- auto install LSP servers from file 'tools/astronvim/lsp/servers.lua'
+  -- https://github.com/AstroNvim/AstroNvim/issues/412
   automatic_installation = true,
 }
-
