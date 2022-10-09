47d656c65d313881e51eae0bd94119ad53b19ea5
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2022-10-09 09:28:50 -0600

    chore(astronvim:filetypes): load file `.yamllint` as a `yaml` file
      ## what
      - add filetype `.yaml` to file `.yamllint`
    
      ## how
    
      ## why
    
      ## where
      - ./tools/astronvim/extras/filetypes.lua
    
      ## usage

--- tools/astronvim/extras/filetypes.lua
+++ tools/astronvim/extras/filetypes.lua
@@ -6,7 +6,7 @@
 -- obtained from
 -- https://code.mehalter.com/projects/68/files/master/init.lua
 -- https://github.com/datamonsterr/astronvim_config/blob/main/polish.lua
--- 
+--
 -- Read more about setting filetypes in Neovim
 -- https://www.lua.org/manual/5.1/manual.html#5.4.1
 -- https://neovim.io/news/2022/04#filetypelua
@@ -16,7 +16,8 @@ vim.filetype.add({
   filename = {
     ["Brewfile"] = "ruby",
     -- ["pi-hole.conf"] = "yaml",
-    ["dockerfile"] = "dockerfile"
+    ["dockerfile"] = "dockerfile",
+    [".yamllint"] = "yaml",
   },
   pattern = {
     [".*%.tmux.conf"] = "tmux",       -- files in dotfiles/tools/tmux/ folder
