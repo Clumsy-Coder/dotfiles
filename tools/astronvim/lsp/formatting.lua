47a5e94f8d156258ab426a147ea8152ab847ee6e
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2022-10-13 09:48:55 -0600

    refactor(astronvim:lsp): change how to disable format on save
      ## what
      - remove file `on_attach.lua`
      - add file `formatting.lua`
      - return table with property `format_on_save`
    
      ## how
      - check https://astronvim.github.io/nightly/Recipes/advanced_lsp#disable-auto-format-on-save
    
      ## why
      - Changes made to Astronvim requires changes to be made
    
      ## where
      - ./tools/astronvim/lsp/on_attach.lua -> ./tools/astronvim/lsp/formatting.lua
    
      ## usage

--- tools/astronvim/lsp/formatting.lua
+++ tools/astronvim/lsp/formatting.lua
@@ -3,8 +3,6 @@
 -- check
 --   https://astronvim.github.io/Recipes/advanced_lsp#disable-auto-format-on-save
 
-return function(client, bufnr)
-  if client.server_capabilities.documentFormattingProvider then
-    vim.api.nvim_del_augroup_by_name("auto_format")
-  end
-end
+return {
+  format_on_save = false
+}
