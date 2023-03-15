f300332a9ce1c34eadeef8f322686c0c020059ec
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2023-03-15 15:17:27 -0600

    chore(astronvim:plugin): move `glow.nvim` config to `tools/astronvim/plugins/glow` folder
      ## what
      - move ./tools/astronvim/plugins/glow.lua -> tools/astronvim/plugins/glow/config.lua
      - add `init.lua` to load plugin using `Lazy.nvim`
    
      ## how
    
      ## why
      - need to change structure to support Astronvim v3
      - Replacing `Packer.nvim` with `Lazy.nvim`
    
      ## where
      - ./tools/astronvim/plugins/glow.lua -> tools/astronvim/plugins/glow/config.lua
      - ./tools/astronvim/plugins/glow/init.lua
    
      ## usage

---
+++ tools/astronvim/plugins/glow/init.lua
@@ -0,0 +1,8 @@
+return {
+  "ellisonleao/glow.nvim",
+  ft = { "markdown", "rmd" },
+  opts = require("user.plugins.glow.config"),
+  -- config = function()
+  --   require("glow").setup()
+  -- end,
+}
