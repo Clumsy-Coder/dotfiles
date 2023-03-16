7ed4c9e911c194e4dc422fad451378d384d00b0f
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2023-03-16 09:38:00 -0600

    chore(astronvim:plugin): move `mason-laspconfig` config to `tools/astronvim/plugins/mason` folder
      ## what
      - move ./tools/astronvim/plugins/mason-lspconfig.lua -> tools/astronvim/plugins/mason/config/mason-lspconfig.lua
      - add entry to `init.lua` to load plugin using `Lazy.nvim`
    
      ## how
      - load astronvim `mason-lspconfig` config
      - load my own `mason-lspconfig` config
      - combine the two configs, using astronvim as the base
      - return the combined configs
    
      ## why
      - need to change structure to support Astronvim v3
      - Replacing `Packer.nvim` with `Lazy.nvim`
    
      ## where
      - ./tools/astronvim/plugins/mason-lspconfig.lua -> tools/astronvim/plugins/mason/config/mason-lspconfig.lua
      - ./tools/astronvim/plugins/mason/init.lua
    
      ## usage

--- tools/astronvim/plugins/mason/init.lua
+++ tools/astronvim/plugins/mason/init.lua
@@ -6,4 +6,11 @@ return {
       return require("astronvim.utils").extend_tbl(opts, config)
     end
   },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    opts = function(_, opts)
+      local config = require("user.plugins.mason.config.mason-lspconfig")
+      return require("astronvim.utils").extend_tbl(opts, config)
+    end
+  },
 }
