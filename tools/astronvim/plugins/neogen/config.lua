ef0ca4eef7382710c4080873b787613fbb0975c8
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2023-03-16 17:59:05 -0600

    style(astronvim:plugin): format plugin `neogen` config to be more readable
      ## what
      - format code to be more readable
    
      ## how
    
      ## why
    
      ## where
      - ./tools/astronvim/plugins/neogen/config.lua
    
      ## usage

--- tools/astronvim/plugins/neogen/config.lua
+++ tools/astronvim/plugins/neogen/config.lua
@@ -8,12 +8,12 @@
 return {
   snippet_engine = "luasnip",
   languages = {
-    javascript = { template = { annotation_convention = "jsdoc" } },
-    javascriptreact = { template = { annotation_convention = "jsdoc" } },
-    lua = { template = { annotation_convention = "ldoc" } },
-    python = { template = { annotation_convention = "google_docstrings" } },
-    sh = { template = { annotation_convention = "google_bash" } },
-    typescript = { template = { annotation_convention = "tsdoc" } },
-    typescriptreact = { template = { annotation_convention = "tsdoc" } },
+    javascript =      { template = { annotation_convention = "jsdoc"              } },
+    javascriptreact = { template = { annotation_convention = "jsdoc"              } },
+    lua =             { template = { annotation_convention = "ldoc"               } },
+    python =          { template = { annotation_convention = "google_docstrings"  } },
+    sh =              { template = { annotation_convention = "google_bash"        } },
+    typescript =      { template = { annotation_convention = "tsdoc"              } },
+    typescriptreact = { template = { annotation_convention = "tsdoc"              } },
   },
 }
