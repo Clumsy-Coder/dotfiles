12ee57e38109c5f306e087dfeafc1e5c3e9c2694
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2023-03-16 09:43:52 -0600

    chore(astornvim:plugin): move `mason-null-ls` config to `tools/astronvim/plugins/mason` folder
      ## what
      - move ./tools/astronvim/plugins/mason-null-ls.lua -> tools/astronvim/plugins/mason/config/mason-null-ls.lua
      - add entry to `init.lua` to load plugin using `Lazy.nvim`
    
      ## how
      - load astronvim `mason-null-ls` config
      - load my own `mason-null-ls` config
      - combine the two configs, using astronvim as the base
      - return the combined configs
    
      ## why
      - need to change structure to support Astronvim v3
      - Replacing `Packer.nvim` with `Lazy.nvim`
    
      ## where
      - ./tools/astronvim/plugins/mason-null-ls.lua -> tools/astronvim/plugins/mason/config/mason-null-ls.lua
      - ./tools/astronvim/plugins/mason/init.lua
    
      ## usage

--- tools/astronvim/plugins/mason/init.lua
+++ tools/astronvim/plugins/mason/init.lua
@@ -13,4 +13,11 @@ return {
       return require("astronvim.utils").extend_tbl(opts, config)
     end
   },
+  {
+    "jay-babu/mason-null-ls.nvim",
+    opts = function(_, opts)
+      local config = require("user.plugins.mason.config.mason-null-ls")
+      return require("astronvim.utils").extend_tbl(opts, config)
+    end
+  }
 }
