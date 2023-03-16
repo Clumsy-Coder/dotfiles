58a087a456a7cef7c792cc206c2d9c6b1acc7dae
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2023-03-16 09:25:46 -0600

    chore(astronvim:plugin): remove plugin `indent-o-matic`
      ## what
      - remove `indent-o-matic` config
    
      ## how
    
      ## why
      - Astronvim replaced it with plugin `guess-indent.nvim`
        - check https://github.com/NMAC427/guess-indent.nvim
    
      ## where
      - ./tools/astronvim/plugins/indent-o-matic.lua
    
      ## usage

--- tools/astronvim/plugins/indent-o-matic.lua
+++
@@ -1,7 +0,0 @@
--- Config: indent-o-matic
--- Darazaki/indent-o-matic
-
-return {
-  -- Skip multi-line comments and strings (more accurate detection but less performant)
-  skip_multiline = true,
-}
