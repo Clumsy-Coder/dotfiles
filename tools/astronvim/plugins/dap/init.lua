da4893c02bae74a3245f3386d9f1d31de5df02c9
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2023-03-16 18:52:42 -0600

    build(astronvim:plugin): disable plugin `mason-nvim-dap.nvim` `nvim-dap` `nvim-dap-ui`
      ## what
      - disable plugin
        - mason-nvim-dap.nvim
        - nvim-dap
        - nvim-dap-ui
    
      ## how
    
      ## why
      - won't be using these plugins
    
      ## where
      - ./tools/astronvim/plugins/dap/init.lua
    
      ## usage

---
+++ tools/astronvim/plugins/dap/init.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "jay-babu/mason-nvim-dap.nvim",
+    enabled = false,
+  },
+  {
+    "mfussenegger/nvim-dap",
+    enabled = false,
+  },
+  {
+    "rcarriga/nvim-dap-ui",
+    enabled = false,
+  },
+}
