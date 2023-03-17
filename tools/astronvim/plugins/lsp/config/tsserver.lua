c21d48870a7db8b316eaf265299d4f880976b651
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2023-03-16 18:41:23 -0600

    refactor(astronvim:plugin): load lsp `tsserver` using astronvim v3
      ## what
      - add `tsserver` to be loaded using astronvim v3 configs
    
      ## how
      - check
        - https://code.mehalter.com/AstroNvim_user/~files/f75107090a877bee07237861d29665cd83e9a9e8/plugins/lsp.lua?position=source-3.1-8.5-1
    
      ## why
      - it will cause errors if not using the method provided for astronvim v3
    
      ## where
      - ./tools/astronvim/plugins/lsp/config/tsserver.lua
    
      ## usage

--- tools/astronvim/plugins/lsp/config/tsserver.lua
+++ tools/astronvim/plugins/lsp/config/tsserver.lua
@@ -2,8 +2,8 @@
 -- jose-elias-alvarez/typescript.nvim
 --
 -- obtained from
---    https://code.mehalter.com/projects/68/files/master/plugins/typescript/config.lua
+--    https://code.mehalter.com/AstroNvim_user/~files/f75107090a877bee07237861d29665cd83e9a9e8/plugins/lsp.lua?position=source-3.1-8.5-1
 
 return {
-  server = astronvim.lsp.server_settings("tsserver"),
+  server = require("astronvim.utils.lsp").config("tsserver")
 }
