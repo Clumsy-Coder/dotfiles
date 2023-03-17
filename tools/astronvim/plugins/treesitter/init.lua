8c92ded51736cbb3d5b35b6724ebade141015133
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2023-03-16 19:35:27 -0600

    build(astronvim:plugin): add plugin `nvim-treesitter/nvim-treesitter-context`
      ## what
      - add plugin `nvim-treesitter/nvim-treesitter-context`
    
      ## how
      - add as a dependency of `nvim-treesitter/nvim-treesitter`
      - obtained code from
        - https://code.mehalter.com/AstroNvim_user/~files/f75107090a877bee07237861d29665cd83e9a9e8/plugins/treesitter.lua?position=source-2.1-10.5-1
    
      ## why
      - this plugin will be used to view the context of a large function when scrolling
        - check https://github.com/nvim-treesitter/nvim-treesitter-context#screenshot
    
      ## where
      - ./tools/astronvim/plugins/treesitter/init.lua
    
      ## usage

--- tools/astronvim/plugins/treesitter/init.lua
+++ tools/astronvim/plugins/treesitter/init.lua
@@ -1,7 +1,12 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  opts = function(_, opts)
-    local config = require("user.plugins.treesitter.config")
-    return require("astronvim.utils").extend_tbl(opts, config)
-  end
+  {
+    "nvim-treesitter/nvim-treesitter",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter-context",
+    },
+    opts = function(_, opts)
+      local config = require("user.plugins.treesitter.config")
+      return require("astronvim.utils").extend_tbl(opts, config)
+    end,
+  },
 }
