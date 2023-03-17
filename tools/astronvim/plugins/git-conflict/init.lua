170aa7787b9ac95bdd53a1b88ce521ce45d4a522
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2023-03-16 18:06:49 -0600

    chore(astronvim:plugin): move `git-conflict.nvim` config to `tools/astronvim/plugins/git-conflict` folder
      ## what
      - move ./tools/astronvim/plugins/git-conflict.lua -> tools/astronvim/plugins/git-conflict/config.lua
      - add `init.lua` to load the plugin using `Lazy.nvim`
    
      ## how
      - load astronvim `git-conflict` config
      - load my own `git-conflict` configs
      - combine the two configs, using astronvim as the base
      - return the combined configs
    
      ## why
      - need to change structure to support astronvim v3
      - Replacing `Packer.nvim` with `Lazy.nvim`
    
      ## where
      - ./tools/astronvim/plugins/git-conflict.lua -> tools/astronvim/plugins/git-conflict/config.lua
      - ./tools/astronvim/plugins/git-conflict/init.lua
    
      ## usage

---
+++ tools/astronvim/plugins/git-conflict/init.lua
@@ -0,0 +1,7 @@
+return {
+  "akinsho/git-conflict.nvim",
+  opts = function(_, opts)
+    local config = require("user.plugins.git-conflict.config")
+    return require("astronvim.utils").extend_tbl(opts, config)
+  end,
+}
