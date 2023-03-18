a69b523d18097bebe5723324b12758eb311201e1
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2023-03-18 09:25:19 -0600

    build(astronvim:plugin): add plugin `rebelot/heirline.nvim`
      ## what
      - add plugin `rebelot/heirline.nvim`
      - display vim mode in the statusline
      - display file path breadcrumbs in the winbar
    
      ## how
      - code obtained from
        - https://code.mehalter.com/AstroNvim_user/~files/2d782a1/plugins/heirline.lua?position=source-9.1-26.6-1
        - https://astronvim.com/Recipes/status#replicate-visual-studio-code-winbar
        - https://astronvim.com/Recipes/status#replicate-nvchad-statusline
    
      ## why
      - this plugin will be used to display
        - tabline
        - winbar
        - statusline
    
      ## where
      - ./tools/astronvim/heirline/attributes.lua
      - ./tools/astronvim/plugins/heirline/init.lua
    
      ## usage

---
+++ tools/astronvim/heirline/attributes.lua
@@ -0,0 +1,9 @@
+-- heirline attributes
+-- check
+--   https://astronvim.com/Recipes/status#basic-options
+return {
+  mode = {          -- vim mode
+    bold = true,    -- set text to bold
+                    -- obtained from https://astronvim.com/Recipes/status#replicate-nvchad-statusline
+  },
+}
