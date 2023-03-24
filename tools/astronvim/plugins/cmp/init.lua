205f7c5b8a3536812d59bbb31e3c705ec2c26d99
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2023-03-24 17:53:50 -0600

    build(astronvim:plugin): load `nvim-cmp` config in `Lazy.nvim`
      ## what
      - load `nvim-cmp` config from `./tools/astronvim/plugins/cmp/config/cmp.lua `
    
      ## how
    
      ## why
      - to load custom configs
      - to load custom cmp sources
      - to config cmp styling
      - to config load cmp source `git` for `gitcommit` filetype
    
      ## where
      - ./tools/astronvim/plugins/cmp/init.lua
    
      ## usage

--- tools/astronvim/plugins/cmp/init.lua
+++ tools/astronvim/plugins/cmp/init.lua
@@ -1,5 +1,9 @@
 return {
   {
     "hrsh7th/nvim-cmp",
+    opts = function(_, opts)
+      local config = require("user.plugins.cmp.config.cmp")
+      return require("astronvim.utils").extend_tbl(opts, config)
+    end,
   },
 }
