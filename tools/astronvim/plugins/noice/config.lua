7e7e745f45a0a31ba0d2172b03eae26ae043a890
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2023-03-25 18:20:41 -0600

    build(astronvim:plugin): add plugin `folke/noice.nvim`
      ## what
      - add plugin `folke/noice.nvim`
    
      ## how
    
      ## why
      - this plugin improve neovim UI.
        - use notify.nvim to display cmdline messages
        - display a dialog box when closing an unsaved buffer
    
      ## where
      - ./tools/astronvim/plugins/noice/config.lua
      - ./tools/astronvim/plugins/noice/init.lua
    
      ## usage

---
+++ tools/astronvim/plugins/noice/config.lua
@@ -0,0 +1,68 @@
+return {
+  presets = {
+    bottom_search = true,         -- use a classic bottom cmdline for search
+    -- command_palette = true, -- position the cmdline and popupmenu together
+    long_message_to_split = true, -- long messages will be sent to a split
+  },
+  cmdline = {
+    enabled = true,
+  },
+  lsp = {
+    progress = { enabled = false },
+    hover = { enabled = false },
+    signature = { enabled = false },
+    message = { enabled = false },
+    -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+    override = {
+      ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+      ["vim.lsp.util.stylize_markdown"] = true,
+      ["cmp.entry.get_documentation"] = true,
+    },
+  },
+  -- routes
+  routes = {
+    -- show recording messages
+    -- obtained from
+    --   https://github.com/folke/noice.nvim/wiki/Configuration-Recipes#show-recording-messages
+    {
+      view = "notify",
+      filter = {
+        event = "msg_showmode",
+      },
+    },
+  },
+  -- views
+  views = {
+    -- view cmdline and popupmenu together
+    -- obtained from
+    --   https://github.com/folke/noice.nvim/wiki/Configuration-Recipes#display-the-cmdline-and-popupmenu-together
+    cmdline_popup = {
+      position = {
+        row = "50%",
+        col = "50%",
+      },
+      size = {
+        width = 60,
+        height = "auto",
+      },
+    },
+    popupmenu = {
+      relative = "editor",
+      position = {
+        row = "66%",
+        col = "50%",
+      },
+      size = {
+        width = 60,
+        height = 10,
+      },
+      border = {
+        style = "rounded",
+        padding = { 0, 1 },
+      },
+      win_options = {
+        winhighlight = { Normal = "Normal", FloatBorder = "DiagnosticInfo" },
+      },
+    },
+  },
+}
