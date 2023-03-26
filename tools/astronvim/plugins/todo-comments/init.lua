54d16feb4087c887b82ed6ec946c24c444d24cc7
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2023-03-25 18:12:11 -0600

    build(astronvim:plugin): load plugin `todo-comments.nvim` after opening a file
      ## what
      - load plugin `todo-comments.nvim` after opening a file
    
      ## how
      - used custom event from Astronvim v3
      - check https://astronvim.com/Configuration/autocmds#example-autocommand-usage
    
      ## why
      - It won't load the plugin if the event is not described
      - By default Lazy.nvim will not load the plugin
        - because the config `lazy = true`
    
      ## where
      - ./tools/astronvim/plugins/todo-comments/init.lua
    
      ## usage

--- tools/astronvim/plugins/todo-comments/init.lua
+++ tools/astronvim/plugins/todo-comments/init.lua
@@ -1,5 +1,6 @@
 return {
   "folke/todo-comments.nvim",
+  event = "User AstroFile",
   opts = function(_, opts)
     local config = require("user.plugins.todo-comments.config")
     return require("astronvim.utils").extend_tbl(opts, config)
