4fd19a4fc48198db9077dfd20cf32f04a09caeac
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2023-03-26 10:27:36 -0600

    chore(astronvim:mapping): move `which-key` mappings to `./tools/astronvim/mappings.lua`
      ## what
      - move ./tools/astronvim/which-key/modes/normal.lua -> tools/astronvim/extras/mappings/modes/normal.lua
      - move ./tools/astronvim/which-key/register.lua -> tools/astronvim/mappings.lua
      - update file references
        - `./tools/astronvim/mappings.lua`
          - `local normal_mode_keymaps = require("user.extras.mappings.modes.normal")`
    
      ## how
    
      ## why
      - need to change structure to support Astronvim v3
      - which-key mappings filename has changed
        - check https://astronvim.com/Configuration/v3_migration
    
      ## where
      - ./tools/astronvim/which-key/modes/normal.lua -> tools/astronvim/extras/mappings/modes/normal.lua
      - ./tools/astronvim/which-key/register.lua -> tools/astronvim/mappings.lua
    
      ## usage

--- tools/astronvim/mappings.lua
+++ tools/astronvim/mappings.lua
@@ -1,7 +1,7 @@
 -- Config: which-key
 -- folke/which-key.nvim
 
-local normal_mode_keymaps = require("user.which-key.modes.normal")
+local normal_mode_keymaps = require("user.extras.mappings.modes.normal")
 
 return {
   n = normal_mode_keymaps
