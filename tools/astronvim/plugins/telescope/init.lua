f991fddd1be2f06fd804606728729b88c681a8f3
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2023-03-30 15:04:41 -0600

    build(astronvim:plugin): add plugin `nvim-telescope/telescope-file-browser.nvim`
      ## what
      - add plugin `nvim-telescope/telescope-file-browser.nvim`
    
      ## how
      - only run plugin after plugin `telescope.nvim` has started
    
      ## why
      - used to browse files in a directory using telescope
    
      ## where
      - ./tools/astronvim/plugins/telescope/init.lua
    
      ## usage

--- tools/astronvim/plugins/telescope/init.lua
+++ tools/astronvim/plugins/telescope/init.lua
@@ -1,7 +1,22 @@
 return {
-  "nvim-telescope/telescope.nvim",
-  opts = function(_, opts)
-    local config = require("user.plugins.telescope.config")
-    return require("astronvim.utils").extend_tbl(opts, config)
-  end
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = {
+      "nvim-telescope/telescope-file-browser.nvim",
+    },
+    opts = function(_, opts)
+      local config = require("user.plugins.telescope.config")
+      return require("astronvim.utils").extend_tbl(opts, config)
+    end,
+  },
+  {
+    "nvim-telescope/telescope-file-browser.nvim",
+    dependencies = {
+      "nvim-telescope/telescope.nvim",
+    },
+    config = function()
+      local telescope = require("telescope")
+      telescope.load_extension("file_browser")
+    end,
+  },
 }
