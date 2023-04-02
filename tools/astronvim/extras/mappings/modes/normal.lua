c76b4a26e08448a1dd38355241b413bf9b4c70e9
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2023-04-02 11:02:39 -0600

    chore(astronvim:mapping): disable which-key mapping for plugin `akinsho/git-conflict.nvim`
      ## what
      - disable which-key mappings for plugin `akinsho/git-conflict.nvim`
    
      ## how
    
      ## why
      - wanted to test out plugin `sindrets/diffview.nvim` as an alternative to `akinsho/git-conflict.nvim`
        - check commit 4a7d22c
    
      ## where
      - ./tools/astronvim/extras/mappings/modes/normal.lua
    
      ## usage

--- tools/astronvim/extras/mappings/modes/normal.lua
+++ tools/astronvim/extras/mappings/modes/normal.lua
@@ -6,17 +6,17 @@
 
 return {
   -- git-conflict.nvim
-  ["C"] = {
-    name = "git-conflict",
-    ["c"] = { "<Plug>(git-conflict-ours)", "Take Current" },
-    ["i"] = { "<Plug>(git-conflict-theirs)", "Take Incoming" },
-    ["b"] = { "<Plug>(git-conflict-both)", "Take Both" },
-    ["0"] = { "<Plug>(git-conflict-none)", "Take None" },
-    ["r"] = { "<cmd>GitConflictRefresh<cr>", "Refresh Conflicts" },
-    ["l"] = { "<cmd>GitConflictListQf<cr>", "List Conflicts" },
-    ["]"] = { "<Plug>(git-conflict-next-conflict)",  "Next conflict" },
-    ["["] = { "<Plug>(git-conflict-prev-conflict)",  "Previous conflict" },
-  },
+  -- ["C"] = {
+  --   name = "git-conflict",
+  --   ["c"] = { "<Plug>(git-conflict-ours)", "Take Current" },
+  --   ["i"] = { "<Plug>(git-conflict-theirs)", "Take Incoming" },
+  --   ["b"] = { "<Plug>(git-conflict-both)", "Take Both" },
+  --   ["0"] = { "<Plug>(git-conflict-none)", "Take None" },
+  --   ["r"] = { "<cmd>GitConflictRefresh<cr>", "Refresh Conflicts" },
+  --   ["l"] = { "<cmd>GitConflictListQf<cr>", "List Conflicts" },
+  --   ["]"] = { "<Plug>(git-conflict-next-conflict)",  "Next conflict" },
+  --   ["["] = { "<Plug>(git-conflict-prev-conflict)",  "Previous conflict" },
+  -- },
   -- ############################################################################################ --
   -- ############################################################################################ --
   -- ############################################################################################ --
