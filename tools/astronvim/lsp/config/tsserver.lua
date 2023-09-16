ec3b594e791e5a26b1ec8b80f40c250556ba7eac
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2023-09-15 19:06:11 -0600

    feat(astronvim:lsp): add config for `tsserver`
      ## what
      - add config for `tsserver`
    
      ## how
      - obtained from
        - https://code.mehalter.com/AstroNvim_user/~files/91d8255e/lsp/config/tsserver.lua
    
      ## why
      - better config when coding typescript files
    
      ## where
      - ./tools/astronvim/lsp/config/tsserver.lua
    
      ## usage

---
+++ tools/astronvim/lsp/config/tsserver.lua
@@ -0,0 +1,30 @@
+-- obtained from
+-- https://code.mehalter.com/AstroNvim_user/~files/91d8255e/lsp/config/tsserver.lua
+return {
+  settings = {
+    typescript = {
+      inlayHints = {
+        includeInlayEnumMemberValueHints = true,
+        includeInlayFunctionLikeReturnTypeHints = true,
+        includeInlayFunctionParameterTypeHints = true,
+        includeInlayParameterNameHints = "all",
+        includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+        includeInlayPropertyDeclarationTypeHints = true,
+        includeInlayVariableTypeHints = true,
+        includeInlayVariableTypeHintsWhenTypeMatchesName = false,
+      },
+    },
+    javascript = {
+      inlayHints = {
+        includeInlayEnumMemberValueHints = true,
+        includeInlayFunctionLikeReturnTypeHints = true,
+        includeInlayFunctionParameterTypeHints = true,
+        includeInlayParameterNameHints = "all",
+        includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+        includeInlayPropertyDeclarationTypeHints = true,
+        includeInlayVariableTypeHints = true,
+        includeInlayVariableTypeHintsWhenTypeMatchesName = false,
+      },
+    },
+  },
+}
