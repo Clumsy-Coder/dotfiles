87702f25be6c2f88c22be68ee4d9fc14f6fb1a7b
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2023-11-01 20:31:31 -0600

    chore(astronvim:indent): remove existing indent-blankline config
      ## what
      - remove existing indent-blankline config
    
      ## how
    
      ## why
      - indent-blankline has changed it's API since v3
        - check
          - https://github.com/lukas-reineke/indent-blankline.nvim/wiki/Migrate-to-version-3#configuration-variables
      - the default config from Astronvim is fine
        - check
          - https://docs.astronvim.com/configuration/plugin_defaults/#indent-blanklinenvim
          - https://github.com/AstroNvim/AstroNvim/blob/207170eae39f29b713e1ac204251109c0cc1860b/lua/plugins/ui.lua#L81-L105
    
      ## where
      - ./tools/astronvim/plugins/indent-blankline/config.lua
    
      ## usage

--- tools/astronvim/plugins/indent-blankline/config.lua
+++ tools/astronvim/plugins/indent-blankline/config.lua
@@ -2,18 +2,18 @@
 -- lukas-reineke/indent-blankline.nvim
 
 return {
-  use_treesitter = true,
-  show_current_context = true,
-  show_current_context_start = false,
-  indent_level = 50,
-  filetype_exclude = {
-    'dashboard',
-    'toggleterm',
-    'packer',
-    'help',
-    'NvimTree',
-    'alpha'
-  }
+  -- use_treesitter = true,
+  -- show_current_context = true,
+  -- show_current_context_start = false,
+  -- indent_level = 50,
+  -- filetype_exclude = {
+  --   'dashboard',
+  --   'toggleterm',
+  --   'packer',
+  --   'help',
+  --   'NvimTree',
+  --   'alpha'
+  -- }
 }
 
 -- for more options, search for 'options.' in
