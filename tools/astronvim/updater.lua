c0461aee40cf377c4169a932840639c964a4f3f5
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2024-06-19 09:26:36 -0600

    refactor(astronvim:updator): set version to `v3.45.3`
    set the latest installed version to `v3.45.3`,
    since the latest astronvim version is `v4.10.4`

--- tools/astronvim/updater.lua
+++ tools/astronvim/updater.lua
@@ -5,7 +5,7 @@
 return {
   remote          = "origin",   -- remote to use
   channel         = "stable",  -- "stable" or "nightly"
-  version         = "latest",   -- "latest", tag name, or regex search like "v1.*" to only do updates before v2 (STABLE ONLY)
+  version         = "v3.45.3",   -- "latest", tag name, or regex search like "v1.*" to only do updates before v2 (STABLE ONLY)
   branch          = "main",     -- branch name (NIGHTLY ONLY)
   commit          = nil,        -- commit hash (NIGHTLY ONLY)
   pin_plugins     = nil,        -- nil, true, false (nil will pin plugins on stable only)
