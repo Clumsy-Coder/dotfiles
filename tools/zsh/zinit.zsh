7e79abe5eeab10da9eccebc48e8485ab1f2a16fb
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2024-12-04 09:36:03 -0700

    build(zsh:zinit): add plugin `zsh-users/zsh-completions`
      ## what
      - add plugin `zsh-users/zsh-completions`
      - load the zsh completions
    
      ## how
      - https://www.youtube.com/watch?v=ud7YxC33Z3w&t=508s
    
      ## why
    
      ## where
      - ./tools/zsh/zinit.zsh
    
      ## usage
    
      ## commit id, issue or pull request
    
      ## notes

--- tools/zsh/zinit.zsh
+++ tools/zsh/zinit.zsh
@@ -21,3 +21,8 @@ source "$ZINIT_HOME/zinit.zsh"
 zinit ice depth=1; zinit light romkatv/powerlevel10k  # theme
 
 zinit light zsh-users/zsh-syntax-highlighting         # highlights commands as you type
+zinit light zsh-users/zsh-completions                 # additional completions for ZSH
+
+# load zsh-completions
+autoload -Uz compinit && compinit
+
