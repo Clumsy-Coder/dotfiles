c9993439e789306a2096f9beb8fb1c1a0d36b974
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2024-12-04 09:03:31 -0700

    feat(zsh:zinit): add basic zinit config
      ## what
      - download zinit if not installed
      - run zinit plugin
    
      ## how
      - obtained from
        - https://github.com/dreamsofautonomy/dotfiles/blob/961c435/.zshrc#L36-L41
        - https://www.youtube.com/watch?v=ud7YxC33Z3w&t=110s
    
      ## why
      - this will be used for initializing zinit
    
      ## where
      - ./tools/zsh/zinit.zsh
    
      ## usage
      - source ~/dotfiles/tools/zsh/zinit.zsh
    
      ## commit id, issue or pull request
    
      ## notes

---
+++ tools/zsh/zinit.zsh
@@ -0,0 +1,17 @@
+#!/usr/bin/env zsh
+
+# zinit plugin manager
+# config obtained from
+# - https://www.youtube.com/watch?v=ud7YxC33Z3w
+
+ZINIT_REPO="https://github.com/zdharma-continuum/zinit.git"
+
+# install zinit IF not installed
+if [ ! -d "$ZINIT_HOME" ]; then
+  echo "Downloading zinit repo $ZINIT_REPO"
+  mkdir -p "$(dirname $ZINIT_HOME)"
+  git clone --depth 1 "$ZINIT_REPO" "$ZINIT_HOME"
+fi
+
+source "$ZINIT_HOME/zinit.zsh"
+
