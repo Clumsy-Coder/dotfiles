4eee337d1eb6d5de26ba308b6f6fafc521a934be
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2024-12-07 10:51:14 -0700

    build(zsh:setopt): add config for history management
      ## what
      - add config for history management
        - hist_ignore_all_dups
        - hist_ignore_space
          - don't write to history if there's a space at the beginning of the
            command
            - useful for hiding sensitive information
        - hist_save_no_dups
    
      ## how
      - https://zsh.sourceforge.io/Doc/Release/Options.html#Options
      - https://www.youtube.com/watch?v=ud7YxC33Z3w&t=653s
    
      ## why
      - this is for handling history
    
      ## where
      - ./tools/zsh/setopt.zsh
    
      ## usage
    
      ## commit id, issue or pull request
    
      ## notes

--- tools/zsh/setopt.zsh
+++ tools/zsh/setopt.zsh
@@ -30,8 +30,10 @@ setopt append_history           # Allow multiple terminal sessions to all append
 # setopt extended_history         # save timestamp of command and duration
 setopt inc_append_history       # Add comamnds as they are typed, don't wait until shell exit
 # setopt hist_expire_dups_first   # when trimming history, lose oldest duplicates first
+setopt hist_save_no_dups        # When writing out the history file, older commands that duplicate newer ones are omitted
 setopt hist_ignore_dups         # Do not write events to history that are duplicates of previous events
-# setopt hist_ignore_space        # remove command line from history list when first character on the line is a space
+setopt hist_ignore_all_dups     # If a new command line being added to the history list duplicates an older one, the older command is removed from the list (even if it is not the previous event)
+setopt hist_ignore_space        # remove command line from history list when first character on the line is a space. useful for hiding sensitive information from being saved
 setopt hist_find_no_dups        # When searching history don't display results already cycled through twice
 # setopt hist_reduce_blanks       # Remove extra blanks from each command line being added to history
 setopt hist_verify              # don't execute, just expand history
