f83c15e70d769d7b7b86f62f5bf875bcfc3a95c6
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2024-12-17 12:16:37 -0700

    refactor(zsh:alias): update alias `os` to ignore `.obsidian` directory
      ## what
      - update alias `os` to ignore `.obsidian` directory
    
      ## how
      - https://stackoverflow.com/a/10589822/3053548
    
      ## why
      - this will hide files from `.obsidian` directory
    
      ## where
      - ./tools/zsh/aliases.zsh
    
      ## usage
    
      ## commit id, issue or pull request
    
      ## notes

--- tools/zsh/aliases.zsh
+++ tools/zsh/aliases.zsh
@@ -126,7 +126,8 @@ alias oo='cd $OBSIDIAN_DIR_PATH/mimir/' # navigate to my obsidian vault
 # - fzf : to fuzzy find the files (by filename) and show a preview
 #
 # obtained from
-# - https://stackoverflow.com/a/10589822/3053548
-# - https://chatgpt.com/c/66ec4e75-e294-8011-94af-28a39183e93d
-alias os="find $OBSIDIAN_DIR_PATH/mimir -name '.git*' -prune -o -type f -print | fzf --preview 'bat --style=numbers --color=always --line-range=:500 {}' --bind 'enter:execute(nvim {})+abort'"
+# - ignore directories
+#   - https://stackoverflow.com/a/10589822/3053548
+#   - https://chatgpt.com/c/66ec4e75-e294-8011-94af-28a39183e93d
+alias os="find $OBSIDIAN_DIR_PATH/mimir -name '.git*' -prune -o -name '.obsidian' -prune -o -type f -print | fzf --preview 'bat --style=numbers --color=always --line-range=:500 {}' --bind 'enter:execute(nvim {})+abort'"
 # -------------------------------------------------------------------------------------------------#
