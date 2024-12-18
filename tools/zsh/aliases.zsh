0e85251a75d00dd32677c6de705c78f7dca463a0
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2024-12-18 10:51:22 -0700

    style(zsh:alias): format alias `os` into multiple lines
      ## what
      - format alias `os` into multiple lines
    
      ## how
    
      ## why
      - easier to read and modify
    
      ## where
      - ./tools/zsh/aliases.zsh
    
      ## usage
    
      ## commit id, issue or pull request
    
      ## notes

--- tools/zsh/aliases.zsh
+++ tools/zsh/aliases.zsh
@@ -128,8 +128,21 @@ alias oo='cd $OBSIDIAN_DIR_PATH/mimir/' # navigate to my obsidian vault
 # obtained from
 # - ignore directories
 #   - https://stackoverflow.com/a/10589822/3053548
+#   - https://stackoverflow.com/a/63523300/3053548
 #   - https://chatgpt.com/c/66ec4e75-e294-8011-94af-28a39183e93d
 # - trim part of the filepath in fzf
 #   - https://github.com/junegunn/fzf/issues/1534#issuecomment-476757278
-alias os="find $OBSIDIAN_DIR_PATH/mimir -name '.git*' -prune -o -name '.obsidian' -prune -o -type f -print | fzf --delimiter obsidian/mimir/ --with-nth -1 --preview 'bat --style=numbers --color=always --line-range=:500 {}' --bind 'enter:execute(nvim {})+abort'"
+alias os="
+find $OBSIDIAN_DIR_PATH/mimir \
+\( \
+  -name '.git*' -o \
+  -name '.obsidian' \
+\) \
+-prune -o \
+-type f -print \
+| \
+fzf --delimiter obsidian/mimir/ \
+    --with-nth -1 \
+    --preview 'bat --style=numbers --color=always --line-range=:500 {}' \
+    --bind 'enter:execute(nvim {})+abort'"
 # -------------------------------------------------------------------------------------------------#
