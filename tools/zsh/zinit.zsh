7d1d59d49fc46ff35b18786df7c99163130cc7f3
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2025-08-19 16:56:58 -0600

    chore(zsh:zinit): display the directory location when cloning zinit repo

--- tools/zsh/zinit.zsh
+++ tools/zsh/zinit.zsh
@@ -9,7 +9,7 @@ ZINIT_REPO="https://github.com/zdharma-continuum/zinit.git"
 
 # install zinit IF not installed
 if [ ! -d "$ZINIT_HOME" ]; then
-  echo "Downloading zinit repo $ZINIT_REPO"
+  echo "Downloading zinit repo $ZINIT_REPO to $ZINIT_HOME"
   mkdir -p "$(dirname $ZINIT_HOME)"
   git clone --depth 1 "$ZINIT_REPO" "$ZINIT_HOME"
 fi
