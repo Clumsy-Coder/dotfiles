4e2c4f4dfc13988234866db6bcd6b978758a7099
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2025-12-19 19:21:25 -0700

    chore(neovim): remove `tools/neovim` directory
      ## what
      - remove `tools/neovim` directory
    
      ## how
      - rm -rf  ./tools/neovim
    
      ## why
      - `neovim` directory was being used by dotbot config. Since dotbot is
        now removed, this directory is no longer being used
    
      ## where
      - ./tools/neovim
    
      ## usage
    
      ## commit id, issue or pull request
    
      ## notes

--- tools/neovim/lua/config/theme.lua
+++
@@ -1,9 +0,0 @@
-local status_ok, comment = pcall(require, "gruvbox")
-if not status_ok then
-  error("Unable to load gruvbox.nvim.")
-  return
-end
-
-vim.opt.termguicolors = true
-vim.o.background = "dark" -- or "light" for light mode
-vim.cmd([[colorscheme gruvbox]])
