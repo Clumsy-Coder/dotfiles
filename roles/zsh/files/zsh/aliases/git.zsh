17fbc6cd7408a3f25280ec134aca492fcd0caa70
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2025-12-22 13:35:04 -0700

    refactor(ansible:roles:zsh:files:alias): move git aliases to `roles/zsh/files/zsh/aliases/git.zsh`
      ## what
      - move git aliases to `roles/zsh/files/zsh/aliases/git.zsh`
    
      ## how
    
      ## why
      - the aliases.zsh file is getting too big, need to split into smaller
        files
    
      ## where
      - ./roles/zsh/files/zsh/aliases.zsh
      - ./roles/zsh/files/zsh/aliases/git.zsh
    
      ## usage
    
      ## commit id, issue or pull request
    
      ## notes

---
+++ roles/zsh/files/zsh/aliases/git.zsh
@@ -0,0 +1,12 @@
+#!/usr/bin/env zsh
+
+# -------------------------------------------------------------------------------------------------#
+# Git
+# -------------------------------------------------------------------------------------------------#
+# alias gits='git status'
+# alias gitl='git log'
+# alias gitc='git commit'
+# alias gitgr='git gr'
+# alias gitexcl='git ls-files --others -i --exclude-standard'
+alias lg='lazygit'
+
